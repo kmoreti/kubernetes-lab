751a55400408785fb6630be47676af7331c97a13
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-24 22:22:33 +0000

    adding etcd install and config

--- Vagrant/ubuntu/etcd/install-and-configure-etcd-server.sh
+++ Vagrant/ubuntu/etcd/install-and-configure-etcd-server.sh
@@ -55,8 +55,8 @@ sudo systemctl enable etcd
 sudo systemctl start etcd
 
 # Verification
-sudo ETCDCTL_API=3 etcdctl member list \
-  --endpoints=https://127.0.0.1:2379 \
-  --cacert=/etc/etcd/ca.crt \
-  --cert=/etc/etcd/etcd-server.crt \
-  --key=/etc/etcd/etcd-server.key
+#sudo ETCDCTL_API=3 etcdctl member list \
+#  --endpoints=https://127.0.0.1:2379 \
+#  --cacert=/etc/etcd/ca.crt \
+#  --cert=/etc/etcd/etcd-server.crt \
+#  --key=/etc/etcd/etcd-server.key
