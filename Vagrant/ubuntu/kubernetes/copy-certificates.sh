1ba8594b964a7c02a5ace4db29fb2c76bb86ce96
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-25 00:23:51 +0000

    fixing kubernetes binaries installation

---
+++ Vagrant/ubuntu/kubernetes/copy-certificates.sh
@@ -0,0 +1,8 @@
+MASTER_VAR_LIB_KUBERNETES="/var/lib/kubernetes/"
+
+sudo mkdir -p "$MASTER_VAR_LIB_KUBERNETES"
+
+sudo cp ca.crt ca.key kube-apiserver.crt kube-apiserver.key \
+    service-account.key service-account.crt \
+    etcd-server.key etcd-server.crt \
+    encryption-config.yaml /var/lib/kubernetes/ "$MASTER_VAR_LIB_KUBERNETES"
---
+++ Vagrant/ubuntu/kubernetes/install-kubernetes-binaries.sh
@@ -0,0 +1,1 @@
+sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
