f79163cbed1caba3b3fde005691e55f232c2b2d4
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-28 15:43:08 +0000

    configuring kube services

---
+++ Vagrant/ubuntu/kubernetes/configure-kube-scheduler.sh
@@ -0,0 +1,21 @@
+MASTER_VAR_LIB_KUBERNETES="/var/lib/kubernetes/"
+
+sudo cp kube-scheduler.kubeconfig "$MASTER_VAR_LIB_KUBERNETES"
+
+cat <<EOF | sudo tee /etc/systemd/system/kube-scheduler.service
+[Unit]
+Description=Kubernetes Scheduler
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-scheduler \\
+  --kubeconfig=/var/lib/kubernetes/kube-scheduler.kubeconfig \\
+  --address=127.0.0.1 \\
+  --leader-elect=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
--- Vagrant/ubuntu/kubernetes/copy-certificates.sh
+++
@@ -1,8 +0,0 @@
-MASTER_VAR_LIB_KUBERNETES="/var/lib/kubernetes/"
-
-sudo mkdir -p "$MASTER_VAR_LIB_KUBERNETES"
-
-sudo cp ca.crt ca.key kube-apiserver.crt kube-apiserver.key \
-    service-account.key service-account.crt \
-    etcd-server.key etcd-server.crt \
-    encryption-config.yaml "$MASTER_VAR_LIB_KUBERNETES"
