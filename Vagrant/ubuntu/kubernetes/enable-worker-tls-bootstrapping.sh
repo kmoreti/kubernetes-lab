804cddcbf08cdb43bb7e2101ed04efa245bfc05d
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-29 23:06:18 +0000

    added worker and master tls bootstrapping

---
+++ Vagrant/ubuntu/kubernetes/enable-worker-tls-bootstrapping.sh
@@ -0,0 +1,107 @@
+# Configure Kubelet to TLS Bootstrap
+
+cat <<EOF | sudo tee /var/lib/kubelet/bootstrap-kubeconfig > /dev/null
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority: /var/lib/kubernetes/ca.crt
+    server: https://192.168.5.30:6443
+  name: bootstrap
+contexts:
+- context:
+    cluster: bootstrap
+    user: kubelet-bootstrap
+  name: bootstrap
+current-context: bootstrap
+kind: Config
+preferences: {}
+users:
+- name: kubelet-bootstrap
+  user:
+    token: 07401c.f395accd246ae52d
+EOF
+
+
+# Create Kubelet Config File
+
+cat <<EOF | sudo tee /var/lib/kubelet/kubelet-config.yaml > /dev/null
+kind: KubeletConfiguration
+apiVersion: kubelet.config.k8s.io/v1beta1
+authentication:
+  anonymous:
+    enabled: false
+  webhook:
+    enabled: true
+  x509:
+    clientCAFile: "/var/lib/kubernetes/ca.crt"
+authorization:
+  mode: Webhook
+clusterDomain: "cluster.local"
+clusterDNS:
+  - "10.96.0.10"
+resolvConf: "/run/systemd/resolve/resolv.conf"
+runtimeRequestTimeout: "15m"
+EOF
+
+# Configure Kubelet Service
+
+cat <<EOF | sudo tee /etc/systemd/system/kubelet.service > /dev/null
+[Unit]
+Description=Kubernetes Kubelet
+Documentation=https://github.com/kubernetes/kubernetes
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStart=/usr/local/bin/kubelet \\
+  --bootstrap-kubeconfig="/var/lib/kubelet/bootstrap-kubeconfig" \\
+  --config=/var/lib/kubelet/kubelet-config.yaml \\
+  --image-pull-progress-deadline=2m \\
+  --kubeconfig=/var/lib/kubelet/kubeconfig \\
+  --cert-dir=/var/lib/kubelet/pki/ \\
+  --rotate-certificates=true \\
+  --rotate-server-certificates=true \\
+  --network-plugin=cni \\
+  --register-node=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# Configure the Kubernetes Proxy
+
+sudo mv kube-proxy.kubeconfig /var/lib/kube-proxy/kubeconfig
+
+cat <<EOF | sudo tee /var/lib/kube-proxy/kube-proxy-config.yaml > /dev/null
+kind: KubeProxyConfiguration
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+clientConnection:
+  kubeconfig: "/var/lib/kube-proxy/kubeconfig"
+mode: "iptables"
+clusterCIDR: "192.168.5.0/24"
+EOF
+
+cat <<EOF | sudo tee /etc/systemd/system/kube-proxy.service > /dev/null
+[Unit]
+Description=Kubernetes Kube Proxy
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-proxy \\
+  --config=/var/lib/kube-proxy/kube-proxy-config.yaml
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start the Worker Services
+
+sudo systemctl daemon-reload
+sudo systemctl enable kubelet kube-proxy
+sudo systemctl start kubelet kube-proxy
