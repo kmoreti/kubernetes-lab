1f61fb288dd7cceaa028b126d658c7f30c67743a
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-29 17:57:36 +0000

    added kubeconfig files

--- Vagrant/ubuntu/kubernetes/start-controller-services.sh
+++ Vagrant/ubuntu/kubernetes/start-controller-services.sh
@@ -1,5 +1,3 @@
-#sudo systemctl daemon-reload
-# sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler
-# sudo systemctl start kube-apiserver kube-controller-manager kube-scheduler
-# sudo systemctl enable kube-apiserver
-# sudo systemctl start kube-apiserver
+sudo systemctl daemon-reload
+sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler
+sudo systemctl start kube-apiserver kube-controller-manager kube-scheduler
