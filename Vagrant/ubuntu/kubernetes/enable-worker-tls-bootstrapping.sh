7535506205a4549303ca596aec3316bbe7b03e81
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-30 15:24:22 +0000

    added worker and master tls bootstrapping

--- Vagrant/ubuntu/kubernetes/enable-worker-tls-bootstrapping.sh
+++ Vagrant/ubuntu/kubernetes/enable-worker-tls-bootstrapping.sh
@@ -1,3 +1,18 @@
+WORKER_ETC_CNI_NET_D="/etc/cni/net.d"
+WORKER_ETC_CNI_BIN="/opt/cni/bin"
+WORKER_VAR_LIB_KUBELET="/var/lib/kubelet"
+WORKER_VAR_LIB_KUBE_PROXY="/var/lib/kube-proxy"
+WORKER_VAR_LIB_KUBERNETES="/var/lib/kubernetes"
+WORKER_VAR_RUN_KUBERNETES="/var/run/kubernetes"
+
+sudo mkdir -p \
+  "$WORKER_ETC_CNI_NET_D" \
+  "$WORKER_ETC_CNI_BIN" \
+  "$WORKER_VAR_LIB_KUBELET" \
+  "$WORKER_VAR_LIB_KUBE_PROXY" \
+  "$WORKER_VAR_LIB_KUBERNETES" \
+  "$WORKER_VAR_RUN_KUBERNETES"
+
 # Configure Kubelet to TLS Bootstrap
 
 cat <<EOF | sudo tee /var/lib/kubelet/bootstrap-kubeconfig > /dev/null
