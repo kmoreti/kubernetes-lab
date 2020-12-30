d1d067c838efb56b75fa5e476ca6f12c4bf59b92
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-30 21:07:36 +0000

    added dns add-on

--- Vagrant/ubuntu/kubernetes/enable-worker-tls-bootstrapping.sh
+++ Vagrant/ubuntu/kubernetes/enable-worker-tls-bootstrapping.sh
@@ -1,5 +1,7 @@
+echo "Enabling TLS bootstrapping..."
+
 WORKER_ETC_CNI_NET_D="/etc/cni/net.d"
-WORKER_ETC_CNI_BIN="/opt/cni/bin"
+WORKER_OPT_CNI_BIN="/opt/cni/bin"
 WORKER_VAR_LIB_KUBELET="/var/lib/kubelet"
 WORKER_VAR_LIB_KUBE_PROXY="/var/lib/kube-proxy"
 WORKER_VAR_LIB_KUBERNETES="/var/lib/kubernetes"
@@ -7,7 +9,7 @@ WORKER_VAR_RUN_KUBERNETES="/var/run/kubernetes"
 
 sudo mkdir -p \
   "$WORKER_ETC_CNI_NET_D" \
-  "$WORKER_ETC_CNI_BIN" \
+  "$WORKER_OPT_CNI_BIN" \
   "$WORKER_VAR_LIB_KUBELET" \
   "$WORKER_VAR_LIB_KUBE_PROXY" \
   "$WORKER_VAR_LIB_KUBERNETES" \
@@ -122,3 +124,5 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable kubelet kube-proxy
 sudo systemctl start kubelet kube-proxy
+
+echo "TLS bootstrapping has been enabled."
