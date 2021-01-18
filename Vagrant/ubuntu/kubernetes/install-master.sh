74e01d2cfd828ee05ec6793602ea4a53c99bd768
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2021-01-18 22:55:44 +0000

    Refactoring...

---
+++ Vagrant/ubuntu/kubernetes/install-master.sh
@@ -0,0 +1,7 @@
+export INSTALLATION="/home/$(logname)/installation"
+export SCRIPTS="$INSTALLATION/scripts"
+export CONFIG="$INSTALLATION/config"
+export CERTS="$INSTALLATION/certs"
+export BINARIES="$INSTALLATION/binaries"
+
+chmod +x "$SCRIPTS/*.sh"
